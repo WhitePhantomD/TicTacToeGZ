d184a3d2ef276d5a4c75fbc27a0a2560896509c9
Gabriel <g_zimmermann@yahoo.com>
2022-10-08 19:18:06 +0200

    Method move finished so that p1 and p2 can place X and O ; if won not finished

--- app/src/main/java/com/example/tictactoegz/MainActivity.java
+++ app/src/main/java/com/example/tictactoegz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tictactoegz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -11,31 +12,67 @@ import com.example.tictactoegz.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ActivityMainBinding binding;
+    ImageView imageViewLeftTop,imageViewLeftMid,imageViewLeftBot,imageViewMidTop,imageViewMidMid,imageViewMidBot,imageViewRightTop,imageViewRightMid,imageViewRightBot;
+
+    TicTacToeLogistic tttl = new TicTacToeLogistic();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
+        imageViewLeftTop = findViewById(R.id.imageViewLeftTop);
+        imageViewLeftMid = findViewById(R.id.imageViewLeftMid);
+        imageViewLeftBot = findViewById(R.id.imageViewLeftBot);
+        imageViewMidTop = findViewById(R.id.imageViewMidTop);
+        imageViewMidMid = findViewById(R.id.imageViewMidMid);
+        imageViewMidBot = findViewById(R.id.imageViewMidBot);
+        imageViewRightTop = findViewById(R.id.imageViewRightTop);
+        imageViewRightMid = findViewById(R.id.imageViewRightMid);
+        imageViewRightBot = findViewById(R.id.imageViewRightBot);
 
-        binding.imageViewLeftTop.setOnClickListener(this);
-        binding.imageViewLeftMid.setOnClickListener(this);
-        binding.imageViewLeftBot.setOnClickListener(this);
-        binding.imageViewMidTop.setOnClickListener(this);
-        binding.imageViewMidMid.setOnClickListener(this);
-        binding.imageViewMidBot.setOnClickListener(this);
-        binding.imageViewRightTop.setOnClickListener(this);
-        binding.imageViewRightMid.setOnClickListener(this);
-        binding.imageViewRightBot.setOnClickListener(this);
+        imageViewLeftTop.setOnClickListener(this);
+        imageViewLeftMid.setOnClickListener(this);
+        imageViewLeftBot.setOnClickListener(this);
+        imageViewMidTop.setOnClickListener(this);
+        imageViewMidMid.setOnClickListener(this);
+        imageViewMidBot.setOnClickListener(this);
+        imageViewRightTop.setOnClickListener(this);
+        imageViewRightMid.setOnClickListener(this);
+        imageViewRightBot.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-
-
+        switch (v.getId()){
+            case R.id.imageViewLeftTop:
+                tttl.move(imageViewLeftTop,0);
+                break;
+            case R.id.imageViewLeftMid:
+                tttl.move(imageViewLeftMid,3);
+                break;
+            case R.id.imageViewLeftBot:
+                tttl.move(imageViewLeftBot,6);
+                break;
+            case R.id.imageViewMidTop:
+                tttl.move(imageViewMidTop,1);
+                break;
+            case R.id.imageViewMidMid:
+                tttl.move(imageViewMidMid,4);
+                break;
+            case R.id.imageViewMidBot:
+                tttl.move(imageViewMidBot,7);
+                break;
+            case R.id.imageViewRightTop:
+                tttl.move(imageViewRightTop,2);
+                break;
+            case R.id.imageViewRightMid:
+                tttl.move(imageViewRightMid,5);
+                break;
+            case R.id.imageViewRightBot:
+                tttl.move(imageViewRightBot,8);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/tictactoegz/TicTacToeLogistic.java
+++ app/src/main/java/com/example/tictactoegz/TicTacToeLogistic.java
@@ -1,31 +1,42 @@
 package com.example.tictactoegz;
 
+import android.widget.ImageView;
+
 public class TicTacToeLogistic {
     //1 == X | 2 == O
-    int[] tttArray = {0,0,0,0,0,0,0,0,0};
-    boolean[] taken = {false,false,false,false,false,false,false,false,false};
+    int[] tttArray = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    boolean[] taken = {false, false, false, false, false, false, false, false, false};
 
     int moveCounter = 0;
-    int placeMent = 0;
 
-    public TicTacToeLogistic(int placeMent,int moveCounter) {
-        this.placeMent = placeMent;
-        this.moveCounter = moveCounter;
+    public TicTacToeLogistic() {
 
     }
 
-    //checks if the place is taken
-    private boolean ifPlaceTaken(){
-        return false;
+    public void move(ImageView imageView, int placeMent) {
+        if (taken[placeMent] == false) {
+            moveCounter++;
+            if (playerMove() == 1) {
+                imageView.setImageResource(R.drawable.image_x);
+            } else {
+                imageView.setImageResource(R.drawable.image_o);
+            }
+            tttArray[placeMent] = playerMove();
+            taken[placeMent] = true;
+        }
     }
 
     //tells what player makes the move
-    private int playerMove(){
-        return 0;
+    private int playerMove() {
+        if(moveCounter % 2 == 0){
+            return 2;
+        } else {
+            return 1;
+        }
     }
 
     //checks if one player won
-    public boolean ifWon(){
+    public boolean ifWon() {
         return false;
     }
 }
